2f3ad31da634069680c5e70fe1906d3bc6dba567
Alex Sharoff <707030+alexsharoff@users.noreply.github.com>
2020-08-21 17:07:28 +0700

    fix line endings, print test logs to stdout

--- bin/test.bat
+++ bin/test.bat
@@ -4,4 +4,4 @@ if "%INCLUDE%" == "" (
     exit /b 1
 )
 set "SCRIPT_DIR=%~dp0"
-python -m unittest discover -v -s "%SCRIPT_DIR%\..\tests" >build_migrator_tests.log 2>&1
+python -m unittest discover -v -s "%SCRIPT_DIR%\..\tests"
--- bin/test.sh
+++ bin/test.sh
@@ -2,4 +2,4 @@
 
 set -e
 SCRIPT_DIR=`dirname "$0"`
-python -m unittest discover -v -s "$SCRIPT_DIR/../tests" &> build_migrator_tests.log
+python -m unittest discover -v -s "$SCRIPT_DIR/../tests"
